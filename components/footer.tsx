18ca63595cfc3a5300875cd143e5341cb4848411
Himanchal Khattri <khattrihimanchal17@gmail.com>
2025-06-18 09:26:06 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="mb-8 sm:mb-10 px-2 sm:px-4 text-center text-gray-500 w-full max-w-full">
       {/* //footer */}
       <small className="mb-2 block text-xs sm:text-sm md:text-base">
-        &copy; 2023. All rights reserved.
+        &copy; 2025. All rights reserved.
       </small>
     </footer>
   );
